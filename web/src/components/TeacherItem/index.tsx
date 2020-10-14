f71d4816de8281306bb61c57c17bd84ef9109df8
TonySDuarte <tonysduarte101@gmail.com>
2020-10-14 11:55:21 -0300

    finish E-3
    Create api using axios, add button in teacherList, front-end connected to back-end,

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,36 +1,53 @@
 import React from 'react'
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+import api from '../../services/api'
 
 import './styles.css'
 
-function TeacherItem(){
-    return(
-        <article className="teacher-item">
-                    <header>
-                        <img src="https://media-exp1.licdn.com/dms/image/C4D03AQFDZF9BIPsdRQ/profile-displayphoto-shrink_200_200/0?e=1607558400&v=beta&t=Hp6iEEfkh7meNjsp0Cqt_eETKV0bY-de91xwKjhvlZM" alt="Tony Sandro"/>
-                        <div>
-                            <strong>Tony Sandro</strong>
-                            <span>Estrutura de dados</span>
-                        </div>
-                    </header>
-                    <p>
-                        Professor de estrutura de dados
-                        <br/> <br/>
-                        Utilizando apenas visualg e uma didatica simples aprenderemos a aplicar em qualquer outra linguagem com facilidade.
-                    </p>
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+  }
+  
+  interface TeacherItemProps {
+    teacher: Teacher;
+  }
 
-                    <footer>
-                        <p>
-                            preço por hora
-                            <strong>R$50,00</strong>
-                        </p>
-                        <button type="button">
-                            <img src={whatsappIcon} alt="whatsapp"/>
-                            Entrar em contato
-                        </button>
-                    </footer>
-                </article>
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection (){
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
+    return (
+        <article className="teacher-item">
+            <header>
+                <img src={teacher.avatar} alt={teacher.name} />
+                <div>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
+                </div>
+            </header>
+
+            <p>{teacher.bio}</p>
+
+            <footer>
+                <p>
+                    preço por hora
+                    <strong>R${teacher.cost}</strong>
+                </p>
+                <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
+                    <img src={whatsappIcon} alt="whatsapp" />
+                    Entrar em contato
+                    </a>
+            </footer>
+        </article>
     )
 }
 
--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,48 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom'
 
-import logoImg from "../../assets/images/logo.svg";
-import landingImg from "../../assets/images/landing.svg";
+import logoImg from '../../assets/images/logo.svg';
+import landingImg from '../../assets/images/landing.svg';
 
-import studyIcon from "../../assets/images/icons/study.svg";
-import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
-import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
+import studyIcon from '../../assets/images/icons/study.svg';
+import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
+import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
-import './styles.css'
+import api from '../../services/api';
+
+import './styles.css';
 
 function Landing() {
-    return (
-        <div id="page-landing">
-            <div id="page-landing-content" className="container">
-                <div id="logo-container">
-                    <img src={logoImg} alt="Proffy"/>
-                    <h2>Sua platafroma de estudos online.</h2>
-                </div>
-                
-                <img 
-                    src={landingImg} 
-                    alt="plataforma de estudo" 
-                    className="hero-image"
-                />
+  const [totalConnections, setTotalConnections] = useState(0);
 
-                <div className="buttons-container">
-                    <Link to="/study" className="study">
-                        <img src={studyIcon} alt="Estudar"/>
-                        estudar
-                    </Link>
+  useEffect(() => {
+    api.get('connections').then(response => {
+      const { total } = response.data; 
 
-                    <Link to="/give-classes" className="give-classes">
-                        <img src={giveClassesIcon} alt="Dar aulas"/>
-                        Dar aulas
-                    </Link>
-                </div>
+      setTotalConnections(total);
+    })
+  }, []);
 
-                <span className="total-connection">
-                    total de 157 conexões já realizadas <img src={purpleHeartIcon} alt="CoraçãoRoxo"/>
-                </span>
-            </div>
+  return (
+    <div id="page-landing">
+      <div id="page-landing-content" className="container">
+        <div className="logo-container">
+          <img src={logoImg} alt="Proffy" />
+          <h2>Sua plataforma de estudos online.</h2>
         </div>
-    )
+
+        <img 
+          src={landingImg} 
+          alt="Plataforma de estudos" 
+          className="hero-image"
+        />
+
+        <div className="buttons-container">
+          <Link to="/study" className="study">
+            <img src={studyIcon} alt="Estudar"/>
+            Estudar
+          </Link>
+
+          <Link to="/give-classes" className="give-classes">
+            <img src={giveClassesIcon} alt="Dar aulas"/>
+            Dar aulas
+          </Link>
+        </div>
+
+        <span className="total-connections">
+          Total de {totalConnections} conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo" />
+        </span>
+      </div>
+    </div>
+  )
 }
- 
+
 export default Landing;
